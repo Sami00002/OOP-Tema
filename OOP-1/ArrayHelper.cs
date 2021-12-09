00a3455a70cb93b2d831d231f8b9401151a2e671
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-09 22:06:30 +0200

    .

--- OOP-1/ArrayHelper.cs
+++ OOP-1/ArrayHelper.cs
@@ -23,7 +23,7 @@ namespace OOP_1
 
                     nrElement++;
 
-                  
+
 
 
                     fib1 = fib2;
@@ -32,6 +32,65 @@ namespace OOP_1
                 }
             }
             return Vector;
+        }
+
+        public static int[] Identitate(int nrElementsr)
+        {
+            Console.Write("Cate numeri vrei sa citesti de la tastatura?");
+            int nrElements = int.Parse(Console.ReadLine());
+            int[] Vector1 = new int[nrElements];
+            int[] Frequencies = new int[nrElements];
+
+
+
+
+
+            for (int i = 0; i < nrElements; i++)
+            {
+                Console.Write("The " + (i + 1) + ". element = ");
+                Vector1[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < nrElements; i++)
+            {
+                Frequencies[i] = 0;
+              
+                for (int j = 0; j < nrElements; j++)
+                    if (Vector1[i] == Vector1[j]) Frequencies[i]++;
+               
+            }
+
+           
+
+            return Frequencies;
+        }
+        public static bool Identitate(int[,] matrix)
+        {
+            bool esteIdentitate = true;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                    if (row != col)
+                    {
+                        if (matrix[row, col] != 0)
+                        {
+                            esteIdentitate = false;
+                        }
+                    }
+                    else if (matrix[row, col] != 1)
+                    {
+                        esteIdentitate = false;
+                    }
+                }
+            }
+            if (esteIdentitate == true)
+                return true;
+            else
+                return false;
         }
     }
 }
--- OOP-1/Program.cs
+++ OOP-1/Program.cs
@@ -6,6 +6,7 @@ namespace OOP_1
     {
         static void Main(string[] args)
         {
+            Console.Write("Cate numere sa fie din sirul fibonacci?");
             int Number;
             Number = Int32.Parse(Console.ReadLine());
 
@@ -13,6 +14,36 @@ namespace OOP_1
 
             for (int i = 0; i < vector.Length; i++)
                 Console.WriteLine(vector[i] + " ");
+            { 
+                Console.Write("\n Cate numeri vrei sa citesti de la tastatura?");
+                int[] Frequencies = ArrayHelper.Identitate(10);
+
+                for (int j = 0; j < Frequencies.Length; j++)
+                {
+                    Console.Write("The " + (j + 1) + ". element appears ");
+                    Console.WriteLine(Frequencies[j] + " times");
+                }
+            }
+
+                Console.WriteLine("\n Introduceti numarul de linii din matrice!");
+
+                int n1 = Int32.Parse(Console.ReadLine());
+                int[,] matrix = new int[n1, n1];
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        Console.Write($"matrix[{row},{col}]:");
+                        matrix[row, col] = Int32.Parse(Console.ReadLine());
+
+                    }
+                }
+                if (ArrayHelper.Identitate(matrix) == true)
+                    Console.WriteLine("Este matrice identitate!");
+                else
+                    Console.WriteLine("Nu este matrice identitate!");
+
+            
         }
     }
 }
