ce9f5d49882dd4bf707167033ad78e37b807cc8f
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-09 21:09:28 +0200

    .

---
+++ OOP-1/ArrayHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    public class ArrayHelper
+    {
+        public static int[] Fibonacci(int Number)
+        {
+            int nrElement = 1;
+
+            int[] Vector = new int[Number];
+            {
+                int fib1 = 1;
+                int fib2 = 1;
+                int fib3 = fib1 + fib2;
+                while (nrElement < Number)
+                {
+                    Vector[nrElement] = fib1;
+
+                    nrElement++;
+
+                  
+
+
+                    fib1 = fib2;
+                    fib2 = fib3;
+                    fib3 = fib1 + fib2;
+                }
+            }
+            return Vector;
+        }
+    }
+}
--- OOP-1/Program.cs
+++ OOP-1/Program.cs
@@ -6,7 +6,13 @@ namespace OOP_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Number;
+            Number = Int32.Parse(Console.ReadLine());
+
+            int[] vector = ArrayHelper.Fibonacci(Number);
+
+            for (int i = 0; i < vector.Length; i++)
+                Console.WriteLine(vector[i] + " ");
         }
     }
 }
