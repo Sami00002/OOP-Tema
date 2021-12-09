3a4a0908f5f635c390feaceb91c629cdd2644db2
Sami00002 <samuel.bodogan@emanuel.ro>
2021-12-09 22:33:24 +0200

    .

---
+++ OOP-1.Tests.NUnit/ArrayHeplerTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace OOP_1.Tests.NUnit
+{
+    public class ArrayHelperTests
+    {
+       
+
+        [Test]
+        public void Numelemetodeipecareotestam_WhenAnumiteconditii_ThanSeintamplaceva()
+        {
+            Assert.Pass();
+        }
+    }
+}
